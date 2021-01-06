0e1ab057efd7a213a1f0b9c602ce352363ea8da5
Nyamu <nyamu.0101@gmail.com>
2021-01-06 18:56:15 +0800

    created graphql directory

--- index.js
+++ index.js
@@ -1,34 +1,9 @@
 const { ApolloServer } = require("apollo-server");
-const gql = require("graphql-tag");
 const mongoose = require("mongoose");
 
 const { MONGODB } = require("./config");
-const Post = require("./models/Post");
-const User = require("./models/User");
-
-const typeDefs = gql`
-  type Post {
-    id: ID!
-    body: String!
-    createdAt: String!
-    username: String!
-  }
-  type Query {
-    getPosts: [Post]
-  }
-`;
-const resolvers = {
-  Query: {
-    async getPosts() {
-      try {
-        const posts = await Post.find();
-        return posts;
-      } catch (e) {
-        throw new Error(e);
-      }
-    },
-  },
-};
+const typeDefs = require("./graphql/typeDefs");
+const resolvers = require("./graphql/resolvers");
 
 const server = new ApolloServer({
   typeDefs,
