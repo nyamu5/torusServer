ea2f31ef8eb9f41e9e9e5972655cc3e3d9b01861
Nyamu <nyamu.0101@gmail.com>
2021-01-10 02:31:03 +0800

    implement comment creation

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -6,6 +6,19 @@ module.exports = typeDefs = gql`
     body: String!
     createdAt: String!
     username: String!
+    comments: [Comment]!
+    likes: [Like]!
+  }
+  type Comment {
+    id: ID!
+    body: String!
+    createdAt: String!
+    username: String!
+  }
+  type Like {
+    id: ID!
+    createdAt: String!
+    username: String!
   }
   type User {
     id: ID!
@@ -29,5 +42,8 @@ module.exports = typeDefs = gql`
     login(username: String!, password: String!): User!
     createPost(body: String!): Post!
     deletePost(postId: ID!): String!
+    createComment(postId: ID!, body: String!): Post!
+    deleteComment(postId: ID!, commentId: ID!): Post!
+    likePost(postId: ID!): Post!
   }
 `;
